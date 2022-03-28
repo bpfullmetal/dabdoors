941164605d53a0b6aa154b9fb980e75647ec8f3a
Xiaofeng <guo.xiaofeng900@hotmail.com>
2022-03-28 17:42:20 +0800

    feat: update the windows settings component

--- wp-content/themes/hoot-ubix-premium/src/ProductBuilder/SettingsComponents/WindowsSettingComponent.js
+++ wp-content/themes/hoot-ubix-premium/src/ProductBuilder/SettingsComponents/WindowsSettingComponent.js
@@ -1,7 +1,14 @@
-const { render, useState } = wp.element;
+
+import { useState, useEffect } from 'react';
+
 import Switch from "react-switch";
 
-const WindowsSettingComponent = ({ additional_price, properties, hasWindow, onChange }) => {
+const WindowsSettingComponent = ({ additional_price, properties, hasWindow, onChange, windowRowsCols }) => {
+  console.log(windowRowsCols);
+  const [cols, setCols] = useState(windowRowsCols?windowRowsCols.cols:4);
+  useEffect(() => {
+    setCols(windowRowsCols.cols);
+  }, [windowRowsCols])
   return (
     <div className="product-setting-item-component window-settings">
       <div class="d-flex align-items-center justify-content-between">
@@ -14,6 +21,16 @@ const WindowsSettingComponent = ({ additional_price, properties, hasWindow, onCh
       <span className="description">
         Click on a window space to add or delete windows.
       </span>
+      <div className={`window-layout-settings mt-1 ${hasWindow ? '' : 'disabled'}`}>
+        <select>
+          <option>None</option>
+          <option disabled={cols!==8}>405 Williamsburg</option>
+          <option disabled={cols!==4 && cols !== 8}>305 Williamsburg</option>
+          <option disabled={cols!==2 && cols!==4 && cols !== 8}>306 Sherwood</option>
+          <option disabled>306 Sherwood</option>
+          <option disabled>306 Sherwood</option>
+        </select>
+      </div>
     </div>
   );
 }
