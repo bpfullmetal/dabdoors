0362e15278b3189e4cf826beacfcc34a5db4429e
Xiaofeng <guo.xiaofeng900@hotmail.com>
2022-03-31 21:22:02 +0800

    feat: update the pack component

--- wp-content/themes/hoot-ubix-premium/src/ProductBuilder/CustomProductComponents/PackComponents.js
+++ wp-content/themes/hoot-ubix-premium/src/ProductBuilder/CustomProductComponents/PackComponents.js
@@ -3,7 +3,7 @@ const { render, useState, useEffect } = wp.element;
 const PackComponent = ({ cols, layoutOption, windowIndex, pack, packItems }) => {
   return (
     <>
-        <img src={packItems[windowIndex % pack]} />
+        <img src={packItems[windowIndex % pack]} className="window-layout-img" />
     </>
   )
 }
