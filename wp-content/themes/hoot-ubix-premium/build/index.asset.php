b1a5f8099838fbbc3cd322934b65c6551de8a4ce
Full Metal Workshop <brandon@fullmetalworkshop.com>
2022-11-18 14:48:12 -0500

    build styles

--- wp-content/themes/hoot-ubix-premium/build/index.asset.php
+++ wp-content/themes/hoot-ubix-premium/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-element'), 'version' => 'c90227138fdf017a2c3b');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-element'), 'version' => 'a88067f44a611c123775');
