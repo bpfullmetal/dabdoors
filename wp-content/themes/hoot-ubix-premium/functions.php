77cbf4eab4fb11b5a0bfff4ebce470ec0af7fca9
Full Metal Workshop <brandon@fullmetalworkshop.com>
2022-11-18 15:15:24 -0500

    change to stylesheet directory

--- wp-content/themes/hoot-ubix-premium/functions.php
+++ wp-content/themes/hoot-ubix-premium/functions.php
@@ -286,11 +286,11 @@ function my_theme_enqueue_styles() {
   if ( is_singular('product') ) {
     wp_enqueue_style(
       'product-builder',
-      get_template_directory_uri() . '/build/style-index.css'
+      get_stylesheet_directory_uri() . '/build/style-index.css'
     );
     wp_enqueue_script(
       'dab-frontend',
-      get_template_directory_uri() . '/build/index.js',
+      get_stylesheet_directory_uri() . '/build/index.js',
       ['wp-element'],
       time(), //For production use wp_get_theme()->get('Version'),
       true
