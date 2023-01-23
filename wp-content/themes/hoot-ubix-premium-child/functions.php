d0e602a1a1ad738f0068a655921ff43831ad48e5
Full Metal Workshop <brandon@fullmetalworkshop.com>
2023-01-23 17:05:22 -0500

    move enqueue js function to child

--- wp-content/themes/hoot-ubix-premium-child/functions.php
+++ wp-content/themes/hoot-ubix-premium-child/functions.php
@@ -1,2 +1,19 @@
 <?php
 
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+function my_theme_enqueue_styles() {
+  if ( is_singular('product') ) {
+    wp_enqueue_style(
+      'product-builder',
+      get_template_directory_uri() . '/build/style-index.css'
+    );
+    wp_enqueue_script(
+      'dab-frontend',
+      get_template_directory_uri() . '/build/index.js',
+      ['wp-element'],
+      time(), //For production use wp_get_theme()->get('Version'),
+      true
+    );
+  }
+}
--- wp-content/themes/hoot-ubix-premium/functions.php
+++ wp-content/themes/hoot-ubix-premium/functions.php
@@ -283,19 +283,6 @@ function my_theme_enqueue_styles() {
     time(), //For production use wp_get_theme()->get('Version'),
     true
   );
-  if ( is_singular('product') ) {
-    wp_enqueue_style(
-      'product-builder',
-      get_template_directory_uri() . '/build/style-index.css'
-    );
-    wp_enqueue_script(
-      'dab-frontend',
-      get_template_directory_uri() . '/build/index.js',
-      ['wp-element'],
-      time(), //For production use wp_get_theme()->get('Version'),
-      true
-    );
-  }
 }
 
 
